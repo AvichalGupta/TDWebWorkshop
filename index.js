650139a4cc19bf45b1250e7c27fed8504b45ca7e
Avichal Gupta <avichalgupta1999@gmail.com>
2021-03-22 20:12:48 +0300

    minor changes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static("icons"))
-app.use(express.static("illustrations"))
-app.use(express.static("images"))
+app.use(express.static(__dirname+"/icons"))
+app.use(express.static(__dirname+"/illustrations"))
+app.use(express.static(__dirname+"/images"))
 
 app.get("/",(req,res)=>{
     res.sendFile(__dirname+"/index.html");
@@ -11,5 +11,6 @@ app.get("/",(req,res)=>{
 
 app.listen(process.env.PORT||3000,()=>{
     console.log("Server running on port,"+process.env.PORT);
+    console.log(__dirname)
 })
 
