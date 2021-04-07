e78817c6a7adb0c29b8d89116acc6767257453e2
Avichal Gupta <avichalgupta1999@gmail.com>
2021-04-07 17:06:17 +0300

    aws changes

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ app.get("/",(req,res)=>{
     res.sendFile(__dirname+"/index.html");
 })
 
-app.listen(process.env.PORT||3000,()=>{
-    console.log("Server running on port,"+process.env.PORT);
+app.listen(3000,()=>{
     console.log("Server running on port, 3000");
 })
 
