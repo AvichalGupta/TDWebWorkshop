bdcff6bc68d66d0c4edf1c26110359c0593dc244
Avichal Gupta <avichalgupta1999@gmail.com>
2021-04-14 10:03:45 +0300

    new link added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get("/",(req,res)=>{
     res.sendFile(__dirname+"/index.html");
 })
 
-app.listen(3000,()=>{
-    console.log("Server running on port, 3000");
+app.listen(process.env.PORT||3000,()=>{
+    console.log("Server running on port,",process.env.PORT||3000);
 })
 
